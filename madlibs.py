a5e895b5edb4d2d76f89ad09e664b74355dc550b
Abdullah Yusuf <normalstuff2001@gmail.com>
2023-03-06 01:07:55 -0800

    Added madlibs game

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+name = str(input("Enter your name: "))
+color = str(input("Enter your favorite color: "))
+food = str(input("Enter your favourite food: "))
+
+print("Your name is " + name)
+print("You are a nigerian and you prefer color "+ color)
+print("Lemme rap for you.....")
+print("Roses are "+ color + " violent are blue but you can't do without "+ food)
